961c54da711f31a57010c91a4e2827bf3aa3aa54
unknown <watterlm@WATTERLM-1.rose-hulman.edu>
2015-01-25 14:43:37 -0500

    Changed main to fragment activity.

---
+++ app/src/main/java/edu/rosehulman/breadcrumb/BookmarkSummary.java
@@ -0,0 +1,14 @@
+package edu.rosehulman.breadcrumb;
+
+import android.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by watterlm on 1/25/2015.
+ */
+public class BookmarkSummary extends Fragment implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
