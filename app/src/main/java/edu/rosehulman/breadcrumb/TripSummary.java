8baabf37d1ce595ed04e35cd4b6630ed413eb633
randman2011 <randallsturner13@gmail.com>
2015-01-25 20:48:28 -0500

    Added inflator.

--- app/src/main/java/edu/rosehulman/breadcrumb/TripSummary.java
+++ app/src/main/java/edu/rosehulman/breadcrumb/TripSummary.java
@@ -1,12 +1,24 @@
 package edu.rosehulman.breadcrumb;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by turnerrs on 1/25/2015.
  */
 public class TripSummary extends Fragment implements View.OnClickListener {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_trip_summary, container, false);
+
+        return view;
+    }
     @Override
     public void onClick(View v) {
 
