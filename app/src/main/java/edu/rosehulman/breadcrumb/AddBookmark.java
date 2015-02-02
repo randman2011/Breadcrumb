84c0542956e966ecc06bd99d00a342c74af30830
randman2011 <randallsturner13@gmail.com>
2015-02-01 19:53:48 -0500

    Implemented Image Picker, which displays two most recent chosen images.

--- app/src/main/java/edu/rosehulman/breadcrumb/AddBookmark.java
+++ app/src/main/java/edu/rosehulman/breadcrumb/AddBookmark.java
@@ -1,6 +1,13 @@
 package edu.rosehulman.breadcrumb;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,17 +16,28 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
 
 /**
  * Created by turnerrs on 1/25/2015.
  */
 public class AddBookmark extends Fragment implements View.OnClickListener {
 
-    private ImageButton addImageButton;
+    private int numAddedImages = 0;
+    private static final int MAX_NUMBER_OF_IMAGES = 10;
+    private Bitmap[] imageBitmaps;
+    private ImageView imageView1;
+    private ImageView imageView2;
     private LinearLayout imageSampler;
     private EditText bookmarkNameText;
     private EditText bookmarkDescriptionText;
+    private static final int KEY_PHOTO_SELECT = 20;
 
     @Nullable
     @Override
@@ -29,16 +47,68 @@ public class AddBookmark extends Fragment implements View.OnClickListener {
         imageSampler = (LinearLayout)view.findViewById(R.id.image_sampler);
         bookmarkNameText = (EditText)view.findViewById(R.id.bookmark_name);
         bookmarkDescriptionText = (EditText)view.findViewById(R.id.bookmark_description);
-        addImageButton = (ImageButton)view.findViewById(R.id.image_add_button);
-        addImageButton.setOnClickListener(this);
+        ((ImageButton)view.findViewById(R.id.image_add_button)).setOnClickListener(this);
         ((Button)view.findViewById(R.id.save_button)).setOnClickListener(this);
         ((Button)view.findViewById(R.id.cancel_button)).setOnClickListener(this);
+        imageView1 = (ImageView)view.findViewById(R.id.imageView1);
+        imageView2 = (ImageView)view.findViewById(R.id.imageView2);
+        imageBitmaps = new Bitmap[MAX_NUMBER_OF_IMAGES];
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cancel_button:
+                // Send signal to MainActivity to close this in FragmentManager
+                return;
+            case R.id.save_button:
+                // Save bookmark to database
+                return;
+            case R.id.image_add_button:
+                // Open up image picker
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, KEY_PHOTO_SELECT);
+                // Add ImageView for added image and add to LinearLayout
+                    // Should it be an ImageButton to allow deleting?
+                if (numAddedImages > 1) {
+                    imageView1.setImageBitmap(imageBitmaps[numAddedImages-2]);
+                    imageView1.setVisibility(View.VISIBLE);
+                    imageView2.setImageBitmap(imageBitmaps[numAddedImages-1]);
+                    imageView2.setVisibility(View.VISIBLE);
+                } else if (numAddedImages == 1) {
+                    imageView2.setImageBitmap(imageBitmaps[numAddedImages-1]);
+                    imageView2.setVisibility(View.VISIBLE);
+                    imageView1.setVisibility(View.GONE);
+                } else {
+                    imageView1.setVisibility(View.GONE);
+                    imageView2.setVisibility(View.GONE);
+                }
 
+                return;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case KEY_PHOTO_SELECT:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri image = data.getData();
+                    InputStream imageStream;
+                    try {
+                        imageStream = this.getActivity().getContentResolver().openInputStream(image);
+                    } catch (FileNotFoundException e) {
+                        Toast.makeText(this.getActivity(), "File not found", Toast.LENGTH_LONG);
+                        return;
+                    }
+                    imageBitmaps[numAddedImages] = BitmapFactory.decodeStream(imageStream);
+                    numAddedImages++;
+
+                }
+        }
     }
 }
